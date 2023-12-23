863251db3cf79fd753f404b9a6da1470fa2269b7
Henok Tesfahun <heni.me.tes@gmail.com>
2023-12-23 16:54:38 +0300

    update

--- src/pages/Learn.tsx
+++ src/pages/Learn.tsx
@@ -1,12 +1,84 @@
-import { Box, Toolbar, Typography } from "@mui/material"
+import { Box, Card, Grid, Toolbar, Typography } from '@mui/material'
+import U from '../assets/U.jpg'
+import ለ from '../assets/ለ.jpg'
+import ሐ from '../assets/ሐ.jpg'
+import መ from '../assets/መ.jpg'
+import ሠ from '../assets/ሠ.jpg'
+import ረ from '../assets/ረ.jpg'
+import ሰ from '../assets/ሰ.jpg'
+import ሸ from '../assets/ሸ.jpg'
+import ቀ from '../assets/ቀ.jpg'
+import በ from '../assets/በ.jpg'
+
+const images = [
+  { image: U, text: 'U(Ha)' },
+  { image: ለ, text: 'ለ(Le)' },
+  { image: ሐ, text: 'ሐ(Hameru Ha)' },
+  { image: መ, text: 'መ(Me)' },
+  { image: ሠ, text: 'ሠ(Se)' },
+  { image: ረ, text: 'ረ(Re)' },
+  { image: ሰ, text: 'ሰ(Se)' },
+  { image: ሸ, text: 'ሸ(She)' },
+  { image: ቀ, text: 'ቀ(Ke)' },
+  { image: በ, text: 'በ(Be)' },
+]
 
 export default function Learn() {
-	return (
-		<>
-			<Box component="main" sx={{ p: 3 }}>
-				<Toolbar />
-				<Typography>Ready to learn</Typography>
-			</Box>
-		</>
-	)
+  function ImageCard({ image, text }: { image: any; text: string }) {
+    return (
+      <Grid item xs={12} sm={6} md={3}>
+        <Card
+          sx={{
+            borderRadius: 3,
+            height: { xs: 500, sm: 700 },
+            position: 'relative',
+          }}
+        >
+          <Box
+            component='img'
+            src={image}
+            sx={{
+              height: { xs: 500, sm: 700 },
+              width: '100%',
+              borderRadius: 3,
+            }}
+          />
+
+          <Box
+            sx={{
+              position: 'absolute',
+              bgcolor: 'black',
+              opacity: 0.5,
+              width: '100%',
+              bottom: 0,
+              borderBottomRightRadius: 3,
+              borderBottomLeftRadius: 3,
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: { xs: 30, sm: 40 },
+                fontWeight: 'bold',
+                color: 'white',
+                px: 2,
+                opacity: 1,
+              }}
+            >
+              {text}
+            </Typography>
+          </Box>
+        </Card>
+      </Grid>
+    )
+  }
+
+  return (
+    <Box sx={{ p: { xs: 2, sm: 5 } }}>
+      <Grid container spacing={3}>
+        {images?.map((item) => (
+          <ImageCard image={item?.image} text={item?.text} />
+        ))}
+      </Grid>
+    </Box>
+  )
 }
--- src/pages/Nav.tsx
+++ src/pages/Nav.tsx
@@ -1,109 +1,140 @@
-import * as React from "react"
-import AppBar from "@mui/material/AppBar"
-import Box from "@mui/material/Box"
-import CssBaseline from "@mui/material/CssBaseline"
-import Toolbar from "@mui/material/Toolbar"
-import Typography from "@mui/material/Typography"
-import Button from "@mui/material/Button"
-import Divider from "@mui/material/Divider"
-import Drawer from "@mui/material/Drawer"
-import IconButton from "@mui/material/IconButton"
-import MenuIcon from "@mui/icons-material/Menu"
-import List from "@mui/material/List"
-import ListItem from "@mui/material/ListItem"
-import ListItemButton from "@mui/material/ListItemButton"
-import ListItemText from "@mui/material/ListItemText"
-import { Link } from "@mui/material"
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
+import Drawer from '@mui/material/Drawer'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemText from '@mui/material/ListItemText'
+import { Link } from '@mui/material'
 // import { Link } from "react-router-dom"
 
 const drawerWidth = 240
 
 export default function Nav() {
-	const [mobileOpen, setMobileOpen] = React.useState(false)
+  const [mobileOpen, setMobileOpen] = React.useState(false)
 
-	const handleDrawerToggle = () => {
-		setMobileOpen((prevState) => !prevState)
-	}
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState)
+  }
 
-	const drawer = (
-		<Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-			<Typography variant="h6" sx={{ my: 2 }}>
-				GRNA
-			</Typography>
-			<Divider />
-			<List>
-				<ListItem disablePadding>
-					<ListItemButton sx={{ textAlign: "center" }} href="/">
-						<ListItemText primary="Home" />
-					</ListItemButton>
-				</ListItem>
-				<ListItem disablePadding>
-					<ListItemButton sx={{ textAlign: "center" }} href="/learn">
-						<ListItemText primary="Learn" />
-					</ListItemButton>
-				</ListItem>
-				<ListItem disablePadding>
-					<ListItemButton sx={{ textAlign: "center" }} href="/hgr">
-						<ListItemText primary="HGR" />
-					</ListItemButton>
-				</ListItem>
-			</List>
-		</Box>
-	)
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography
+        variant='h6'
+        sx={{
+          my: 2,
+          fontSize: { xs: 30, sm: 50, md: 65 },
+          fontWeight: 'bold',
+          background:
+            'var(--newww, linear-gradient(90deg, #63B4FF 0.21%, #002977 99.79%))',
+          backgroundClip: 'text',
+          '-webkit-background-clip': 'text',
+          '-webkit-text-fill-color': 'transparent',
+        }}
+      >
+        GRNA
+      </Typography>
+      <Divider />
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton sx={{ textAlign: 'center' }} href='/'>
+            <ListItemText primary='Home' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton sx={{ textAlign: 'center' }} href='/learn'>
+            <ListItemText primary='Learn' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton sx={{ textAlign: 'center' }} href='/hgr'>
+            <ListItemText primary='HGR' />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  )
 
-	return (
-		<Box sx={{ display: "flex" }}>
-			<CssBaseline />
-			<AppBar component="nav">
-				<Toolbar>
-					<IconButton
-						color="inherit"
-						aria-label="open drawer"
-						edge="start"
-						onClick={handleDrawerToggle}
-						sx={{ mr: 2, display: { sm: "none" } }}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Typography
-						variant="h6"
-						component="div"
-						sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-					>
-						GRNA
-					</Typography>
-					<Box sx={{ display: { xs: "none", sm: "block" } }}>
-						<Button href="/" color="inherit">
-							Home
-						</Button>
-						<Button href="learn" color="inherit">
-							Learn
-						</Button>
-						<Button href="hgr" color="inherit">
-							HGR
-						</Button>
-					</Box>
-				</Toolbar>
-			</AppBar>
-			<nav>
-				<Drawer
-					variant="temporary"
-					open={mobileOpen}
-					onClose={handleDrawerToggle}
-					ModalProps={{
-						keepMounted: true, // Better open performance on mobile.
-					}}
-					sx={{
-						display: { xs: "block", sm: "none" },
-						"& .MuiDrawer-paper": {
-							boxSizing: "border-box",
-							width: drawerWidth,
-						},
-					}}
-				>
-					{drawer}
-				</Drawer>
-			</nav>
-		</Box>
-	)
+  return (
+    <Box sx={{ display: 'flex', px: 3, py: 1 }}>
+      <CssBaseline />
+      <Box sx={{ width: '100%' }}>
+        <Toolbar sx={{ width: '100%' }}>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            edge='start'
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              width: '100%',
+            }}
+          >
+            <Typography
+              variant='h6'
+              component='div'
+              sx={{
+                flexGrow: 1,
+                display: { xs: 'none', sm: 'block' },
+                fontSize: { xs: 30, sm: 50 },
+                fontWeight: 'bold',
+                background:
+                  'var(--newww, linear-gradient(90deg, #63B4FF 0.21%, #002977 99.79%))',
+                backgroundClip: 'text',
+                '-webkit-background-clip': 'text',
+                '-webkit-text-fill-color': 'transparent',
+              }}
+            >
+              GRNA
+            </Typography>
+            <Box sx={{ display: { xs: 'none', sm: 'flex' }, gap: 5 }}>
+              <Button href='/' color='inherit'>
+                <Typography sx={{ fontWeight: 'bold' }}>Home</Typography>
+              </Button>
+              <Button href='learn' color='inherit'>
+                <Typography sx={{ fontWeight: 'bold' }}>Learn</Typography>
+              </Button>
+              <Button href='hgr' color='inherit'>
+                <Typography sx={{ fontWeight: 'bold' }}>HGR</Typography>
+              </Button>
+            </Box>
+          </Box>
+        </Toolbar>
+      </Box>
+      <nav>
+        <Drawer
+          variant='temporary'
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </Box>
+  )
 }
